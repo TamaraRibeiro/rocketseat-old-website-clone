53b941911c216d514e3a111b4b07206b6e8fc727
TamaraRibeiro <tamara.fl@hotmail.com>
2024-05-20 19:48:03 -0300

    add rocketseat's color to theme; add About section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ['Inter', 'sans-serif'],
+			serif: ['Merriweather', 'serif'],
+		  },
+		extend: {
+			colors: {
+				rocketseat: "#7159C1"
+			},
+		},
 	},
 	plugins: [],
 }
